4f0ca4c8b5c4474f57687f39d537788ee380186e
pswai <parksoon_my@hotmail.com>
2017-05-12 18:05:06 +0800

    Migrate `not-detected` component to React

---
+++ app/components/not-detected.jsx
@@ -0,0 +1,33 @@
+import React from 'npm:react';
+import PropTypes from 'npm:prop-types';
+
+const NotDetected = (props) => {
+  const { description, reasonsTitle, children } = props;
+
+  return (
+    <div className="error-page js-error-page">
+      <div className="error-page__content">
+        <div className="error-page__header">
+          <div className="error-page__title js-error-page-title">{description} not detected!</div>
+        </div>
+        <div className="error-page__reasons">
+          <div className="error-page__reasons-title">
+            {reasonsTitle ? reasonsTitle : 'Here are some common reasons this happens:'}
+          </div>
+          <ul className="error-page__list">
+            {children}
+          </ul>
+          If you're still having trouble, please file an issue on the Ember Inspector's
+          <a href="https://github.com/emberjs/ember-inspector" target="_blank">GitHub page.</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+NotDetected.propTypes = {
+  description: PropTypes.string.isRequired,
+  reasonsTitle: PropTypes.string
+};
+
+export default NotDetected;
